b8f32118e03c503b6ffce457c6e4b9c629a8e4ff
Mariana <marianadiscipula@gmail.com>
2022-08-04 20:04:39 -0300

    Transformando animaNumeros em classe e tirando uma função do tooltip

--- js/modules/initAnimaNumeros.js
+++ js/modules/initAnimaNumeros.js
@@ -1,38 +1,57 @@
-export default function initAnimaNumeros() {
+export default class AnimaNumeros {
+    constructor(numeros, observerTarget, observerClass) {
+        this.numeros = document.querySelectorAll(numeros);
+        this.observerClass = observerClass;
 
-    function animaNumeros() {
-
-        const numeros = document.querySelectorAll('[data-numero]');
-
-        numeros.forEach(numero => {
-            const total = +numero.innerText;
-
-            let start = 0;
-            let incremento = Math.floor(total / 100);
-
-            const timer = setInterval(() => {
-                start += incremento;
-                numero.innerText = start;
-
-                if (start > total) {
-                    numero.innerText = total;
-                    clearInterval(timer);
-                }
-            }, 25 * Math.random());
-        })
+        this.observerTarget = document.querySelector(observerTarget);
+        this.handleMutation = this.handleMutation.bind(this);
     }
 
+    //Recebe um elemento do dom e incrementa a apartir de 0 até o número final
 
-    function handleMutation(mutation) {
-        if (mutation[0].target.classList.contains('ativo')) {
-            observer.disconnect();
-            animaNumeros()
+    static incrementarNumero(numero) {
+        const total = +numero.innerText;
+
+        let start = 0;
+        let incremento = Math.floor(total / 100);
+
+        const timer = setInterval(() => {
+            start += incremento;
+            numero.innerText = start;
+
+            if (start > total) {
+                numero.innerText = total;
+                clearInterval(timer);
+            }
+        }, 25 * Math.random());
+    };
+
+    animaNumeros() {
+        this.numeros.forEach(numero => this.constructor.incrementarNumero(numero))
+    };
+
+
+    handleMutation(mutation) {
+        if (mutation[0].target.classList.contains(this.observerClass)) {
+            this.observer.disconnect();
+            this.animaNumeros()
         };
-    }
+    };
 
-    const observerTarget = document.querySelector('.numeros');
-    const observer = new MutationObserver(handleMutation);
 
-    observer.observe(observerTarget, { attributes: true });
+    //Adiciona o MutationObserver para verificar quando a classe é adicionada ao elemento
+
+    addMutationObserver() {
+        this.observer = new MutationObserver(this.handleMutation);
+        this.observer.observe(this.observerTarget, { attributes: true });
+    };
+
+    init() {
+        if (this.numeros.length && this.observerTarget) {
+            this.addMutationObserver();
+        }
+        return this;
+    };
+
 }
 
