f28678898c2bc7dc12e23706989219008ec36f01
Mariana <marianadiscipula@gmail.com>
2022-08-05 08:30:02 -0300

    fetch-bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,13 +1,10 @@
-export default function initFetchBitcoin() {
+export default function fetchBitcoin(url, target) {
 
+    fetch(url)
+        .then(res => res.json())
+        .then(bitcoin => {
+            const btcPreco = document.querySelector(target);
+            btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
 
-
+        }).catch(erro => console.log(Error(erro)))
 };
-
-fetch('https://blockchain.info/ticker')
-    .then(res => res.json())
-    .then(bitcoin => {
-        const btcPreco = document.querySelector('.btc-preco');
-        btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
-
-    }).catch(erro => console.log(Error(erro)))
